07a3c77691534f6d5ae5d30df91ddf6b41440a0f
fabio98p <parisifabio98@gmail.com>
2021-03-15 18:42:56 +0100

    simon finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,21 +4,60 @@
 console.log("ciao Ale");
 
 //genera 5 numeri casualmente es. da 1 a 10
-var correctNumber = []
-var number = ""
+var randomNumber = [];
+var number = "";
 for (let i = 0; i < 5; i++) {
 	do {
-		number = random(1, 10+1);
-	} while (correctNumber.includes(number));
-	correctNumber.push(number);
+		number = random(1, 10 + 1);
+	} while (randomNumber.includes(number));
+	randomNumber.push(number);
 }
-console.log(correctNumber);
+console.log(randomNumber, "randomnumber");
 //stampa i 5 numeri sul html e avvia un timer di 30s o 3 per comodita
-//chiedi tramite prompt di inserire tutti i numeri trasmessi in precedenza
-//controllo che i numeri siano corretti
-//stampare numeri iniziali, i numeri scritti e infine quali erano corretti o e quali no
+$(".begin").click(function () {
+	$(".random-numbers").html(randomNumber.toString())
+	setTimeout(function () {
+		$(".random-numbers").html("")
+		setTimeout(function () {
+			//chiedi tramite prompt di inserire tutti i numeri trasmessi in precedenza e fare il controllo del caso
+			//rubato malissimo al progetto campominato
+			var playerNumber = []
+			var userNumber
+			var observer = true
+
+			for (let i = 1; i <= 5 ; i++) {
+				observer = true
+				//controllo che i numeri siano corretti
+				do {
+					if (observer) {
+						observer = false
+						userNumber = parseInt(prompt("scrivi un numero da 1 a 10", "1"));
+					}
+					else {
+						userNumber = parseInt(prompt("hai sbagliato a scrivere, scrivi un altro numero da 1 a 10", "1"));
+					}
+				} while (playerNumber.includes(userNumber) || isNaN(userNumber) || userNumber < 1 || userNumber >= 10);
+				playerNumber.push(userNumber)
+			}
+			//stampare numeri iniziali, i numeri scritti e infine quali erano corretti o e quali no, in questo caso li distinguo tra rosso e verde
+			var risultato = 0
+			for (let i = 1; i <= 5 ; i++) {
+				if (randomNumber.includes(playerNumber[i-1])) {
+					$(".result-numbers").append(`<div class="indovinato">${i},</div>`);
+					risultato++
+				}else{
+					$(".result-numbers").append(`<div class="fallito">${i},</div>`);
+				}
+			}
+			$(".random-numbers").html(randomNumber.toString())
+			$(".player-numbers").html(playerNumber.toString())
+			$(".result").html(`Hai azzeccato ${risultato} numeri`)
+		}, 3000)
+	}, 3000)
+});
+
 
 
 function random(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min; 
+	return Math.floor(Math.random() * (max - min)) + min;
 }
